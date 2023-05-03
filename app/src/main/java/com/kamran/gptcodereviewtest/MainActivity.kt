4c81fb4ed773da797544f3c2a197f90ca09082e0
Kamran Shahid <kamiy2j@gmail.com>
2023-05-04 00:09:15 +0500

    Added yml and badly optimize grade calculator function

--- app/src/main/java/com/kamran/gptcodereviewtest/MainActivity.kt
+++ app/src/main/java/com/kamran/gptcodereviewtest/MainActivity.kt
@@ -44,3 +44,17 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+fun calculateGrade(score: Int): String {
+    if (score >= 90 && score <= 100) {
+        return "A"
+    } else if (score >= 80 && score < 90) {
+        return "B"
+    } else if (score >= 70 && score < 80) {
+        return "C"
+    } else if (score >= 60 && score < 70) {
+        return "D"
+    } else {
+        return "F"
+    }
+}
