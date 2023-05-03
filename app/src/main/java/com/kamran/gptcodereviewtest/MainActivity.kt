14d681474453c9b49d9a2dbbdd97fb57335e8e64
Kamran Shahid <kamiy2j@gmail.com>
2023-05-04 00:54:40 +0500

    added methods to test code review openai

--- app/src/main/java/com/kamran/gptcodereviewtest/MainActivity.kt
+++ app/src/main/java/com/kamran/gptcodereviewtest/MainActivity.kt
@@ -44,3 +44,54 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+// Code Review Test
+
+
+fun modifyList(list: MutableList<Int>) {
+    list.add(42)
+}
+
+fun divide(x: Int, y: Int): Int {
+    var result = 0
+    try {
+        result = x / y
+    } catch (e: Exception) {
+        // Do nothing
+    }
+    return result
+}
+
+fun printArray(array: Array<Int>) {
+    var i = 0
+    for (i in 0 until array.size) {
+        println(array[i])
+    }
+    println("The array has ${i} elements")
+}
+
+var counter = 0
+fun incrementCounter() {
+    counter++
+}
+
+fun getGrade(score: Int): String {
+    if (score >= 90) {
+        return "A"
+    } else if (score >= 80) {
+        return "B"
+    } else if (score >= 70) {
+        return "C"
+    } else if (score >= 60) {
+        return "D"
+    } else {
+        return "F"
+    }
+}
+
+fun divideZero(x: Int, y: Int): Int {
+    if (y == 0) {
+        throw RuntimeException("Cannot divide by zero")
+    }
+    return x / y
+}
